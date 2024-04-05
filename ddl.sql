17330c1431241641645181ccb6170e0240f59261
Jackie Smolkin-Lerner <jackie@smallcity.ca>
2024-04-05 12:22:10 -0400

    Add matches.json

--- ddl.sql
+++ ddl.sql
@@ -1,20 +1,36 @@
-drop table if exists competitions cascade;
-drop table if exists seasons cascade;
+drop table if exists 
+competitions,
+seasons,
+countries,
+managers,
+competition_stages,
+stadiums,
+referees,
+matches,
+teams,
+team_managers,
+position,
+players
+cascade;
 
 create table if not exists competitions (
-	competition_id int not null primary key,
+	competition_id int not null,
+	season_id int not null,
+	season_name varchar(30) not null,
 	country_name varchar(30) not null, -- TODO: Reference countries
 	competition_name varchar(30) not null,
 	competition_gender varchar(8),
 	competition_youth boolean,
-	competition_international boolean
+	competition_international boolean,
+	primary key (competition_id, season_id)
 );
 
+/*
 create table if not exists seasons (
 	season_id int not null,
-	season_name varchar(10) not null,
-	competition_id int references competitions(competition_id)
+	season_name varchar(30) not null,
 );
+*/
 
 create table if not exists countries (
 	country_id int primary key,
@@ -23,7 +39,7 @@ create table if not exists countries (
 
 create table if not exists managers (
 	manager_id int primary key,
-	name varchar(30),
+	name varchar(40),
 	nickname varchar(30),
 	dob date,
 	country_id int references countries(country_id)
@@ -31,35 +47,21 @@ create table if not exists managers (
 
 create table if not exists competition_stages (
 	competition_stage_id int primary key,
-	name varchar(10)
+	name varchar(30)
 );
 
 create table if not exists stadiums (
 	stadium_id int primary key,
-	name varchar(30),
+	name varchar(80),
 	country_id int references countries(country_id)
 );
 
 create table if not exists referees (
 	referee_id int primary key,
-	name varchar(30),
+	name varchar(40),
 	country_id int references countries(country_id)
 );
-create table if not exists matches (
-	match_id int not null primary key,
-	match_date date,
-	kick_off time,
-	competition int references competitions(competition_id),
-	season int references seasons(season_id),
-	home_team_id int references teams(team_id),
-	away_team_id int references teams(team_id),
-	home_score int,
-	away_score int,
-	match_week int,
-	competition_stage int references competition_stages(competition_stage_id),
-	stadium_id int references stadiums(stadium_id),
-	referee_id int references referees(referee_id)
-);
+
 
 create table if not exists teams (
 	team_id int primary key,
@@ -68,6 +70,24 @@ create table if not exists teams (
 	team_group int, -- TODO: Figure out what this is
 	country_id int references countries(country_id)
 );
+
+create table if not exists matches (
+	match_id int not null primary key,
+	match_date date,
+	kick_off time,
+	competition_id int not null,
+	season_id int not null,
+	home_team_id int references teams(team_id),
+	away_team_id int references teams(team_id),
+	home_score int,
+	away_score int,
+	match_week int,
+	competition_stage_id int references competition_stages(competition_stage_id),
+	stadium_id int references stadiums(stadium_id),
+	referee_id int references referees(referee_id),
+	foreign key (competition_id, season_id) references competitions(competition_id, season_id)
+);
+
 
 create table if not exists team_managers (
 	team_id int references teams(team_id),
--- json_loader/main.py
+++ json_loader/main.py
@@ -20,22 +20,73 @@ SEASONS: list[str] = [
 COLUMNS = {
     "competitions": (
         "competition_id",
+        "season_id",
+        "season_name",
         "country_name",
         "competition_name",
         "competition_gender",
         "competition_youth",
         "competition_international",
     ),
-    "seasons": (
-        "season_id",
-        "season_name",
+    # "seasons": (
+    # ),
+    "countries": (
+        "country_id",
+        "name",
+    ),
+    "managers": (
+        "manager_id",
+        "name",
+        "nickname",
+        "dob",
+        "country_id",
+    ),
+    "team_managers": (
+        "team_id",
+        "manager_id",
+    ),
+    "competition_stages": (
+        "competition_stage_id",
+        "name",
+    ),
+    "stadiums": (
+        "stadium_id",
+        "name",
+        "country_id",
+    ),
+    "referees": (
+        "referee_id",
+        "name",
+        "country_id",
+    ),
+    "matches": (
+        "match_id",
+        "match_date",
+        "kick_off",
         "competition_id",
-    )
+        "season_id",
+        "home_team_id",
+        "away_team_id",
+        "home_score",
+        "away_score",
+        "match_week",
+        "competition_stage_id",
+        "stadium_id",
+        "referee_id",
+    ),
+    "teams": (
+        "team_id",
+        "team_name",
+        "team_gender",
+        "team_group",
+        "country_id",
+    ),
 }
 
 # TODO: Delete before submission
 """ SCRATCH PAD
 Tables:
+competitions
 seasons
 countries
 managers
@@ -47,7 +98,6 @@ teams
 team_managers
 position
 players
-
 """
 
 def insert(cursor, table, tuples):
@@ -59,7 +109,16 @@ def insert(cursor, table, tuples):
                         VALUES ({', '.join(['%s'] * len(columns))})
                         """, record)
 
+def remove_duplicates_from_tuples(tuples: list[tuple]):
+    seen_ids = set()
+    def unique(tup: tuple) -> bool:
+        if tup[0] in seen_ids:
+            return False
 
+        seen_ids.add(tup[0])
+        return True
+
+    return filter(unique, tuples)
 
 def main():
     if not os.path.isdir(DATA_PATH):
@@ -69,14 +128,15 @@ def main():
     with psycopg.connect(f"dbname={DATABASE} user={USERNAME} password={PASSWORD}") as conn:
         with conn.cursor() as cursor:
             competition_ids = set()
+            season_ids = set()
             competition_tuples = []
-            season_tuples = []
+            # season_tuples = []
 
             # TODO: This shouldn't be run everytime - remove when script is done
             with open("./ddl.sql", 'r') as ddl:
                 cursor.execute(ddl.read()) # type: ignore
 
-            with open(f"{DATA_PATH}competitions.json", "r") as f:
+            with open(f"{DATA_PATH}competitions.json", "r", encoding="utf-8") as f:
                 data = json.load(f)
                 for row in data:
 
@@ -84,40 +144,170 @@ def main():
                     if row['season_name'] not in SEASONS:
                         continue
 
-                    if row['competition_id'] not in competition_ids:
-                        competition_tuples.append((
-                            row['competition_id'],
-                            row['country_name'],
-                            row['competition_name'],
-                            row['competition_gender'],
-                            row['competition_youth'],
-                            row['competition_international'],
-                        ))
-
-                        competition_ids.add(row['competition_id'])
-
-                    season_tuples.append((
+                    competition_tuples.append((
+                        row['competition_id'],
                         row['season_id'],
                         row['season_name'],
-                        row['competition_id'],
+                        row['country_name'],
+                        row['competition_name'],
+                        row['competition_gender'],
+                        row['competition_youth'],
+                        row['competition_international'],
                     ))
 
-            insert(cursor, "competitions",  competition_tuples)
-            insert(cursor, "seasons",  season_tuples)
+                    competition_ids.add(row['competition_id'])
+                    season_ids.add(row['season_id'])
 
+                    # season_tuples.append((
+                    #     row['season_id'],
+                    #     row['season_name'],
+                    #     row['competition_id'],
+                    # ))
+
+            insert(cursor, "competitions",  competition_tuples)
+            # insert(cursor, "seasons",  season_tuples) 
 
             # Build list of json files with matches data
-            # matches_paths = []
-            # for id in competition_ids:
-            #     with os.scandir(f"{DATA_PATH}matches/{id}") as dirs:
-            #         for path in dirs:
-            #             matches_paths.append(path.path)
-            #
-            #
-            # for path in matches_paths:
-            #     with open(path, "r") as f:
-            #         data = json.load(f)
-            #         for match in data:
+            matches_paths = []
+            for id in competition_ids:
+                with os.scandir(f"{DATA_PATH}matches/{id}") as dirs:
+                    for path in dirs:
+                        if int(path.name.split(".")[0]) in season_ids:
+                            matches_paths.append(path.path)
+
+            country_tuples = []
+            manager_tuples = []
+            competition_stage_tuples = []
+            stadium_tuples = []
+            referee_tuples = []
+            match_tuples = []
+            team_tuples = []
+            team_manager_tuples = []
+
+            for path in matches_paths:
+                with open(path, "r", encoding = "utf-8") as f:
+                    data = json.load(f)
+
+                    for match in data:
+                        for country in (match["home_team"]["country"],
+                                        match["away_team"]["country"],
+                                        ):
+                            country_tuples.append((
+                                country["id"],
+                                country["name"],
+                            ))
+
+                        referee = match.get("referee", None)
+                        if referee is not None:
+                            country_tuples.append((
+                                referee["country"]["id"],
+                                referee["country"]["name"],
+                            ))
+                            referee_tuples.append((
+                                match["referee"]["id"],
+                                match["referee"]["name"],
+                                match["referee"]["country"]["id"],
+                            ))
+                            referee = referee["id"]
+
+                        stadium = match.get("stadium", None)
+                        if stadium is not None:
+                            country_tuples.append((
+                                stadium["country"]["id"],
+                                stadium["country"]["name"],
+                            ))
+                            stadium_tuples.append((
+                                stadium["id"],
+                                stadium["name"],
+                                stadium["country"]["id"],
+                            ))
+                            stadium = stadium["id"]
+
+                        for manager in match["home_team"].get("managers", ()):
+                            manager_tuples.append((
+                                manager["id"],
+                                manager["name"],
+                                manager["nickname"],
+                                manager["dob"],
+                                manager["country"]["id"],
+                            ))
+                            country_tuples.append((
+                                manager["country"]["id"],
+                                manager["country"]["name"],
+                            ))
+                            team_manager_tuples.append((
+                                match["home_team"]["home_team_id"],
+                                manager["id"],
+                            ))
+
+                        for manager in match["away_team"].get("managers", ()):
+                            manager_tuples.append((
+                                manager["id"],
+                                manager["name"],
+                                manager["nickname"],
+                                manager["dob"],
+                                manager["country"]["id"],
+                            ))
+                            country_tuples.append((
+                                manager["country"]["id"],
+                                manager["country"]["name"],
+                            ))
+                            team_manager_tuples.append((
+                                match["away_team"]["away_team_id"],
+                                manager["id"],
+                            ))
+
+                        competition_stage_tuples.append((
+                            match["competition_stage"]["id"],
+                            match["competition_stage"]["name"],
+                        ))
+
+                        team_tuples.append((
+                            match["home_team"]["home_team_id"],
+                            match["home_team"]["home_team_name"],
+                            match["home_team"]["home_team_gender"],
+                            match["home_team"]["home_team_group"],
+                            match["home_team"]["country"]["id"],
+                        ))
+
+                        team_tuples.append((
+                            match["away_team"]["away_team_id"],
+                            match["away_team"]["away_team_name"],
+                            match["away_team"]["away_team_gender"],
+                            match["away_team"]["away_team_group"],
+                            match["away_team"]["country"]["id"],
+                        ))
+
+                        match_tuples.append((
+                            match["match_id"],
+                            match["match_date"],
+                            match["kick_off"],
+                            match["competition"]["competition_id"],
+                            match["season"]["season_id"],
+                            match["home_team"]["home_team_id"],
+                            match["away_team"]["away_team_id"],
+                            match["home_score"],
+                            match["away_score"],
+                            match["match_week"],
+                            match["competition_stage"]["id"],
+                            stadium,
+                            referee,
+                        ))
+
+            insert(cursor, "countries",  remove_duplicates_from_tuples(country_tuples))
+            insert(cursor, "competition_stages",  remove_duplicates_from_tuples(competition_stage_tuples))
+            insert(cursor, "stadiums",  remove_duplicates_from_tuples(stadium_tuples))
+            insert(cursor, "referees",  remove_duplicates_from_tuples(referee_tuples))
+            insert(cursor, "teams",  remove_duplicates_from_tuples(team_tuples))
+            insert(cursor, "managers",  remove_duplicates_from_tuples(manager_tuples))
+            insert(cursor, "team_managers",  remove_duplicates_from_tuples(team_manager_tuples))
+            insert(cursor, "matches",  remove_duplicates_from_tuples(match_tuples))
+
+
+
+
+
+
 
 
 
